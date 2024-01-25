9a953137958ab7522ced5785d81eb942a1acc29d
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-25 22:14:54 +0000

    Initial basic files plus basic News Search html file. API test connection to NY Times.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //---
 // inititalise Console.log()
-Console.log("Console log started:");
+console.log("Console log started:");
 // parameters that will be needed:
 // Search Term
 // Number of Records to Retrieve
@@ -8,3 +8,38 @@ Console.log("Console log started:");
 // End Year
 
 // Display Top Articles
+
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?
+// api-key=V1Z3RWPffGAmoVSNIxa8EkggWQQcG3mV
+// &q=Richard+Branson
+// &begin_date=20000101
+// &end_date=20100101
+
+var baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+var APIKey = "V1Z3RWPffGAmoVSNIxa8EkggWQQcG3mV";
+var searchTerm = "Richard+Branson";
+var begin = 20000101;
+var end = 20100101;
+var queryURL =
+  baseURL +
+  "api-key=" +
+  APIKey +
+  "&q=" +
+  searchTerm +
+  "&begin_date=" +
+  begin +
+  "&end_date=" +
+  end;
+
+console.log(queryURL);
+
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (result) {
+    //
+    //
+
+    console.log(result);
+  });
