fa47df4d4ef25506a9be16eda458bb1554a7affa
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-25 21:30:48 +0000

    loading the template files for the Project.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//---
+// inititalise Console.log()
+Console.log("Console log started:");
+// parameters that will be needed:
+// Search Term
+// Number of Records to Retrieve
+// Start Year
+// End Year
+
+// Display Top Articles
